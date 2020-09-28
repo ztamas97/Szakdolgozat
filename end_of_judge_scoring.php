d44440bdbd521c94a51cc9777277daa4814961f1
Tamas Zelles <zelles.tamas@gmail.com>
2020-09-28 21:46:24 +0200

    fix judge scoring method

--- end_of_judge_scoring.php
+++ end_of_judge_scoring.php
@@ -1,8 +1,8 @@
 <?php
-include("session.php");
+include('session.php');
 
-$team_name=$_SESSION["team_name"];
-$team_id=$_SESSION["team_id"];
+$team_name=$_SESSION['team_name'];
+$team_id=$_SESSION['team_id'];
 if(isset($_POST['bonus_p'])){
 	$bonus=mysqli_real_escape_string($db, $_POST['bonus_p']);
 }
@@ -13,13 +13,13 @@ if(isset($_POST['message_to_team'])){
 	$team_message=mysqli_real_escape_string($db, $_POST['message_to_team']);
 }
 else{
-	$team_message="";
+	$team_message='';
 }
 if(isset($_POST['message'])){
 	$judge_message=mysqli_real_escape_string($db, $_POST['message']);
 }
 else{
-	$judge_message="";
+	$judge_message='';
 }
 
 $sql_sub="SELECT SUM(`zsuri-pontok`.`ERTEK`) AS SUMPOINT
@@ -30,26 +30,26 @@ $sql_sub="SELECT SUM(`zsuri-pontok`.`ERTEK`) AS SUMPOINT
 $result_sub = $db->query($sql_sub);
 $row_sub = $result_sub->fetch_assoc();
 
-$sumofpoint = $row_sub["SUMPOINT"]+$bonus;
+$sumofpoint = $row_sub['SUMPOINT']+$bonus;
 echo $sumofpoint;
 $sql="INSERT INTO `zsuri-osszesito` (`OAZ`, `CSID`, `OSSZPONT`, `PLUSSZP`, `ZSDM`, `MEGJ`, `RIDO`, `FID`, `ZSKATID`) VALUES (NULL, '$team_id', '$sumofpoint', '$bonus', '$judge_message', '$team_message', CURRENT_TIMESTAMP, '$user_tourn', '$user_category_id')";
 
 if(mysqli_query($db, $sql)){
-	$describe="Eredmény véglegesítve: ".$team_name." ".$user_tourn." ".$user_category;
+	$describe='Eredmény véglegesítve: '.$team_name.' '.$user_tourn.' '.$user_category;
 	$sql_log="INSERT INTO `naplo` (`Azonosito`, `Felhasználó`, `Időpont`, `Esemény`) VALUES (NULL, '$user_name, CURRENT_TIMESTAMP, '$describe')";
     if(mysqli_query($db, $sql_log)){
 	} 
 	else{
 	}
-	header("location: judge_scoring.php");
+	header('location: judge_scoring.php');
 } else{
-	$describe="Eredmény véglegesítése sikertelen: ".$team_name." ".$user_tourn." ".$user_category;
+	$describe='Eredmény véglegesítése sikertelen: '.$team_name.' '.$user_tourn.' '.$user_category;
 	$sql_log="INSERT INTO `naplo` (`Azonosito`, `Felhasználó`, `Időpont`, `Esemény`) VALUES (NULL, '$user_name', CURRENT_TIMESTAMP, '$describe')";
 	if(mysqli_query($db, $sql_log)){
 	} 
 	else{
 	}
-    echo "HIBA: A hozzáadás nem sikeres! $sql. " . mysqli_error($db);
+    echo 'HIBA: A hozzáadás nem sikeres! $sql. ' . mysqli_error($db);
 }
 
 ?>
--- update_scoresheet.php
+++ update_scoresheet.php
@@ -1,29 +1,29 @@
 <?php
-include("session.php");
+include('session.php');
 
 $res=mysqli_real_escape_string($db, $_GET['result_judge']);
 $res_id=mysqli_real_escape_string($db, $_GET['chid']);
 $obj_id=mysqli_real_escape_string($db, $_GET['oid']);
-$team_name=$_SESSION["team_name"];
-$team_id=$_SESSION["team_id"];
+$team_name=$_SESSION['team_name'];
+$team_id=$_SESSION['team_id'];
 
 $sql="UPDATE `zsuri-pontok` SET `ERTEK` = '$res',`TIMES` = CURRENT_TIMESTAMP WHERE `zsuri-pontok`.`PAZ` = '$obj_id'";
 
 if(mysqli_query($db, $sql)){
-	$describe="Eredmény frissítve: ".$team_name." ".$user_tourn." ".$res;
+	$describe='Eredmény frissítve: '.$team_name.' '.$user_tourn.' '.$res;
 	$sql_log="INSERT INTO `naplo` (`Azonosito`, `Felhasználó`, `Időpont`, `Esemény`) VALUES (NULL, '$login_session', CURRENT_TIMESTAMP, '$describe')";
     if(mysqli_query($db, $sql_log)){
 	} 
 	else{
 	}
-	header("location: judge_scoresheet.php#$res_id");
+	header('location: judge_scoresheet.php#$res_id');
 } else{
-	$describe="Eredmény frissítése sikertelen: ".$team_name." ".$user_tourn." ".$res;
+	$describe='Eredmény frissítése sikertelen: '.$team_name.' '.$user_tourn.' '.$res;
 	$sql_log="INSERT INTO `naplo` (`Azonosito`, `Felhasználó`, `Időpont`, `Esemény`) VALUES (NULL, '$login_session', CURRENT_TIMESTAMP, '$describe')";
 	if(mysqli_query($db, $sql_log)){
 	} 
 	else{
 	}
-    echo "HIBA: A hozzáadás nem sikeres! $sql. " . mysqli_error($db);
+    echo 'HIBA: A hozzáadás nem sikeres! $sql. ' . mysqli_error($db);
 }
 ?>
